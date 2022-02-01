7b86549fb7032c47983d0281d637dfe18396c13a
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-01 22:18:10 +0530

    add: added json to support

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,8 @@ fn rocket() -> _ {
             routes::index,
             routes::fetch_all_todo_items,
             routes::add_todo_item,
-            routes::delete_todo_item
+            routes::delete_todo_item,
+            routes::json
         ],
     )
 }
